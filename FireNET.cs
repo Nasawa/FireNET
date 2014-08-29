d304dc4cb6f5188e7c81ba717dfbab3e04cfcdf1
Nasawa <keitonasawa@gmail.com>
2014-08-28 23:13:11 -0500

    Init
    Adding stuffs

---
+++ FireNET.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace FireNET
+{
+    public class FireNET
+    {
+        private String root, current;
+        private System.Net.HttpWebRequest request;
+
+        public FireNET(String URL, String auth = null)
+        {
+            current = root = URL;
+        }
+
+        public String getRoot()
+        {
+            return root;
+        }
+
+        public String getChildren(String child = "")
+        {
+            request = System.Net.WebRequest.Create(current + child + ".json") as System.Net.HttpWebRequest;
+            request.Method = "GET";
+            request.ContentLength = 0;
+            using (var response = request.GetResponse() as System.Net.HttpWebResponse)
+            {
+                using (var reader = new System.IO.StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public String getChild(String child)
+        {
+            return getChildren(child);
+        }
+
+        public void descend(String child)
+        {
+            current += child;
+        }
+
+        public void ascend(String parent)
+        {
+            var t = current.LastIndexOf(parent);
+            current = current.Substring(0, t + parent.Length);
+        }
+    }
+}
